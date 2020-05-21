d0a1496a18fe20c0d0690a9dea0c2c468f4ca713
Thanos Papathanasiou <thanospapathanasiou@hotmail.com>
2020-05-21 16:15:58 +0100

    added a simple lexer and parser
    they actually do simple mathematical operations!

--- src/Interpreter/Interpreter.fs
+++ src/Interpreter/Interpreter.fs
@@ -1,15 +1,9 @@
 ﻿module FSlispy.Interpreter
 
-open System.IO
 open FSharp.Text.Lexing
 open FSlispy.Lexer
 
-let testLexerAndParserFromString (text:string) =
-  let lexbuf = LexBuffer<char>.FromString text
-  let countFromParser = Parser.start tokenstream lexbuf
-    
-  countFromParser
-
 let evaluate (input:string) =
-  let parserOutput = testLexerAndParserFromString input
-  "the word hello was included: " + (string parserOutput) + " times"
+  let lexbuf = LexBuffer<char>.FromString input
+  let output = Parser.main tokenize lexbuf
+  string output
